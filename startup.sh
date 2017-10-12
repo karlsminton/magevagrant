3e7068855fbd51011f42b453da3b18e27c9f2fea
Karl Minton <kminton@rippleffect.com>
2017-10-12 12:33:54 +0100

    Changed from symlink to edit virtualhost - now pointing at directory created during provision

--- startup.sh
+++ startup.sh
@@ -9,6 +9,17 @@ apt-get upgrade -y
 apt-get install php5 -y
 
 apt-get install mysql-server-5.6 -y
-# apache2 already installed
-ln -s /var/www/html /vagrant/public_html
-# mkdir /vagrant/public_html
+mkdir /vagrant/public_html
+
+mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-avaiable/000-default.conf.original
+touch /etc/apache2/sites-available/000-default.conf
+FILE="/etc/apache2/sites-available/000-default.conf"
+
+cat <<EOM > $FILE
+<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+	DocumentRoot /vagrant/public_html
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOM
